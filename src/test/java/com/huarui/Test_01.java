02551566c89ef46a65ab649d3eafba644dbbbf70
lihui <youxiu326@163.com>
2020-12-21 09:57:02 +0800

    commit: first commit

---
+++ src/test/java/com/huarui/Test_01.java
@@ -0,0 +1,14 @@
+package com.huarui;
+
+/**
+ * @Auther: lihui
+ * @Date: 2020-12-21 09:56
+ * @Description:
+ */
+public class Test_01 {
+
+    public static void main(String[] args) {
+        System.out.println("这是我dev分支第一次提交");
+    }
+
+}
