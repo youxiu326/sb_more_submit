6b89ad4e4078dfd313f2c9f5bbc11bacf7d57def
youxiu326 <youxiu326@163.com>
2022-05-05 15:45:16 +0800

    使用lua脚本
    Signed-off-by: lihui <lihui2@jtexpress.com>

--- src/main/java/com/huarui/util/RedisTokenUtils.java
+++ src/main/java/com/huarui/util/RedisTokenUtils.java
@@ -1,6 +1,8 @@
 package com.huarui.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.ReturnType;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -18,6 +20,14 @@ public class RedisTokenUtils {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    private static final String LUASCRIPT =
+            "if redis.call('exists', KEYS[1]) == 0 then " +
+                    "return 0; " +
+                    "else " +
+                    "redis.call('del', KEYS[1]); " +
+                    "return 1; " +
+                    "end;";
+
     /**
      * 获取Token 并将Token保存至redis
      * @return
@@ -33,7 +43,8 @@ public class RedisTokenUtils {
      * @param tokenKey
      * @return
      */
-    public boolean findToken(String tokenKey){
+    @Deprecated
+    public boolean findTokenOld(String tokenKey){
         String token = (String) redisTemplate.opsForValue().get(tokenKey);
         if (StringUtils.isEmpty(token)) {
             return false;
@@ -43,5 +54,21 @@ public class RedisTokenUtils {
         return true;
     }
 
+    /**
+     * 判断Token是否存在 并且删除Token
+     * @param tokenKey
+     * @return
+     */
+    public boolean findToken(String tokenKey){
+        Boolean existKey = (Boolean) redisTemplate.execute(
+                (RedisConnection connection) -> connection.eval(
+                        LUASCRIPT.getBytes(), //lua脚本
+                        ReturnType.BOOLEAN,   //设置返回 布尔值
+                        1, //设置key数量
+                        tokenKey.getBytes()
+                )
+        );
+        return existKey;
+    }
 
-} 
+}
--- src/test/java/com/huarui/SbMoreSubmitApplicationTests.java
+++ src/test/java/com/huarui/SbMoreSubmitApplicationTests.java
@@ -2,15 +2,56 @@ package com.huarui;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SbMoreSubmitApplicationTests {
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	@Test
 	public void contextLoads() {
+		/*
+		if(exists key){
+			return false;
+		}else {
+		 	del key;
+		 	return true;
+		 }
+		 */
+
+		String keys = "token_111";
+
+		String luaScript =
+				"if redis.call('exists', KEYS[1]) == 0 then " +
+						"return 0; " +
+						"else " +
+						"redis.call('del', KEYS[1]); " +
+						"return 1; " +
+						"end;";
+
+		Boolean existKey = findToken(luaScript, keys);
+		System.out.println(existKey);
+	}
+
+
+	public Boolean findToken(String luaScript,String keys)  {
+		Boolean existKey = (Boolean) redisTemplate.execute(
+				(RedisConnection connection) -> connection.eval(
+						luaScript.getBytes(),//lua脚本字符串
+						ReturnType.BOOLEAN,//设置返回值 byte[]
+						1, //设置key数量
+						keys.getBytes()
+				)
+		);
+		return existKey;
 	}
 
 }
